5424f0b7c49ff07350c3bc6d5e69a4310bbcb0b9
Joaquim-Keloglanian <joaquim.keloglanian@gmail.com>
2026-01-11 16:24:55 +0100

    feat(chapter4: reference): Start learning about references after finishing owernship chapter.
    Branch: main

--- ch04-00/ownership/src/main.rs
+++ ch04-00/ownership/src/main.rs
@@ -1,22 +1,11 @@
 fn main() {
-    let s: String = String::from("hello");      // s comes into scope
+    let s1: String = String::from("Hello, world! This is your boy, Skinny Penis!");
+    let (s2, len): (String, usize) = calculate_length(s1);
 
-    takes_ownership(s);                         // the value of s is passed into this function
-                                                // ... and so it is no longer valid
-
-    let x: i32 = 5;                             // x comes into scope
-
-    makes_copy(x);                              // i32 implements the Copy trait,
-                                                // so x does not move into the function
-                                                // so it's okay to use x afterward
+    println!("The length of '{s2}' is {len}.");
 }
 
-fn takes_ownership(some_string: String) -> () { // some_string comes into scope
-    println!("{some_string}");
-}                                               // some_string goes out of scope and 'drop' is called
-                                                // The allocated memory is freed
-
-fn makes_copy(some_integer: i32) -> () {        // some_integer comes into scope
-    println!("{some_integer}");
-}                                               // some_integers goes out of scope
-                                                // it is stored on the stack so nothing special happens here
+fn calculate_length(input_string: String) -> (String, usize) {
+    let length: usize = input_string.len();
+    return (input_string, length);
+}
