4bba35efaae59e2c5c4ca8d3a6ce781d78801d21
Joaquim-Keloglanian <joaquim.keloglanian@gmail.com>
2026-01-11 21:47:21 +0100

    chap4(reference & slice): Finished reference chapter and started slie chapter.
    Branch: main

--- ch04-00/reference/src/main.rs
+++ ch04-00/reference/src/main.rs
@@ -1,11 +1,30 @@
 fn main() {
-    let s1: String = String::from("Hello, world!");
+    let s1: String = String::from("Hello");
 
     let len: usize = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}");
+
+    borrow_example();
+
+    let _reference_to_nothing: String = dangle();
 }
 
 fn calculate_length(s: &String) -> usize {
     return s.len();
+}
+
+fn borrow_example() -> () {
+    let mut s: String = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) -> () {
+    some_string.push_str(", world!");
+}
+
+fn dangle() -> String {
+    let s: String = String::from("hello");
+    return s;
 }
